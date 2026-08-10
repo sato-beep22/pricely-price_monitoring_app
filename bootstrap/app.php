3c3ebc4b41512c1553dafa7a1b641d8217fd69a5
sato-beep22 <amorajehu079@gmail.com>
2026-08-10 21:32:25 +0800

    Trust all proxies to fix CSRF 419 error on Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'phone.verified' => EnsurePhoneVerified::class,
