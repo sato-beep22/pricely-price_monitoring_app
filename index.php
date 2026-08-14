76c23b141020987d3449ab9da7f11dea11777b82
sato-beep22 <amorajehu079@gmail.com>
2026-08-14 13:26:15 +0800

    Add root index.php and fix .htaccess for Hostinger shared hosting

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Laravel - Shared Hosting Entry Point
+ * Routes all requests through Laravel's public/index.php
+ */
+
+$publicPath = __DIR__ . '/public';
+
+chdir($publicPath);
+
+require $publicPath . '/index.php';
